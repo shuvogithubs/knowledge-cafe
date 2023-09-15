30928398f33f1293de3ef14327874cbc6b6e3cfd
shuvo karmokar <99184657+shuvogithubs@users.noreply.github.com>
2023-09-15 14:45:49 +0600

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+
+
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-2xl bg-red-500'>Knowledge Cafe </h1>
+        </div>
+    );
+};
+
+
+
+export default Header;
